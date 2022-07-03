071cc0d3b741afbcde14994cf630d3ccaefb49c2
arina <rina150197@gmail.com>
2022-07-03 15:23:17 +0300

    lastcommit

--- src/js/notes.js
+++ src/js/notes.js
@@ -1,55 +1,150 @@
-const modalTriggerFirst = document.querySelector('[data-modal-1]'), 
-          modalTriggerSecond = document.querySelector('[data-modal-2]'), 
-          modalTriggerThird = document.querySelector('[data-modal-3]');
+document.addEventListener('DOMContentLoaded', () => {
+
+        //parse JSON into JS
+    
+          $.getJSON("js/current-loans.json", function(data) { 
+            console.log(data); 
+    
+            $('.item-name.first').html(data.loans[0].title); 
+            $('.modal__loan-title-first').html(data.loans[0].title); 
+            $('.item-name.second').html(data.loans[1].title); 
+            $('.modal__loan-title-second').html(data.loans[1].title);
+            $('.item-name.third').html(data.loans[2].title); 
+            $('.modal__loan-title-third').html(data.loans[2].title);
+            $('.item-descr.first span').html(data.loans[0].available); 
+            $('.item-descr.second span').html(data.loans[1].available);
+            $('.item-descr.third span').html(data.loans[2].available);
+            $('.item-descr.first span1').html(data.loans[0].ltv); 
+            $('.item-descr.second span1').html(data.loans[1].ltv); 
+            $('.item-descr.third span1').html(data.loans[2].ltv); 
+            $('.modal__available-1 span').html(data.loans[0].available); 
+            $('.modal__available-2 span').html(data.loans[1].available);
+            $('.modal__available-3 span').html(data.loans[2].available);
+            
+        //seconds to days converter
+
+            function secondsToDhms(seconds) {
+                seconds = Number(seconds);
+                var d = Math.floor(seconds / (3600*24));
+                var h = Math.floor(seconds % (3600*24) / 3600);
+                var m = Math.floor(seconds % 3600 / 60);
+                var s = Math.floor(seconds % 60);
+                
+                var dDisplay = d > 0 ? d + (d == 1 ? " day " : " days ") : "";
+                var hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
+                var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+                var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+                return dDisplay + hDisplay + mDisplay + sDisplay;
+                }
+        
+        // convert json term remaining data into modal
+
+            const modalTimer1 = document.querySelector('.modal__timer-1 span'),
+                  modalTimer2 = document.querySelector('.modal__timer-2 span'),
+                  modalTimer3 = document.querySelector('.modal__timer-3 span');
+            
+            modalTimer1.innerHTML = `${secondsToDhms(data.loans[0].term_remaining)}`;
+            modalTimer2.innerHTML = `${secondsToDhms(data.loans[1].term_remaining)}`;
+            modalTimer3.innerHTML = `${secondsToDhms(data.loans[2].term_remaining)}`;
+        });
+
+    //modal
+
+        const   modalTriggerFirst = document.querySelector('[data-btn-1]'), 
+                modalTriggerSecond = document.querySelector('[data-btn-2]'), 
+                modalTriggerThird = document.querySelector('[data-btn-3]'),
+                modal1 = document.querySelector('[data-modal-1]'),
+                modal2 = document.querySelector('[data-modal-2]'),
+                modal3 = document.querySelector('[data-modal-3]'),
+                modalCloseBtn1 = document.querySelector('[data-close-1]'),
+                modalCloseBtn2 = document.querySelector('[data-close-2]'),
+                modalCloseBtn3 = document.querySelector('[data-close-3]'),
+                investedTrigger1 = document.querySelector('[data-investbtn-1]'),
+                investedTrigger2 = document.querySelector('[data-investbtn-2]'),
+                investedTrigger3 = document.querySelector('[data-investbtn-3]'),
+                investInput = document.querySelector('.modal__input'),
+                totalSum = document.querySelector('.investment-amount'),
+                investedTextFirst = document.querySelector('[data-invested-1]'), 
+                investedTextSecond = document.querySelector('[data-invested-2]'), 
+                investedTextThird = document.querySelector('[data-invested-3]');
 
 
+        //show modal
 
-
-          modalTriggerFirst.addEventListener('click', () => { 
-            modal.classList.add('show'); 
-            modal.classList.remove('hide'); 
-            investBtn.classList.add('first'); 
+        modalTriggerFirst.addEventListener('click', () => { 
+            modal1.classList.add('show'); 
+            modal1.classList.remove('hide'); 
         }); 
      
         modalTriggerSecond.addEventListener('click', () => { 
-            modal.classList.add('show'); 
-            modal.classList.remove('hide'); 
-            investBtn.classList.add('second'); 
+            modal2.classList.add('show'); 
+            modal2.classList.remove('hide'); 
         }); 
         modalTriggerThird.addEventListener('click', () => { 
-            modal.classList.add('show'); 
-            modal.classList.remove('hide'); 
-            investBtn.classList.add('third'); 
+            modal3.classList.add('show'); 
+            modal3.classList.remove('hide'); 
         });
 
+        //close modal
+
+        function closeModalFirst() {
+            modal1.classList.add('hide');
+            modal1.classList.remove('show');
+        }
+        function closeModalSecond() {
+            modal2.classList.add('hide');
+            modal2.classList.remove('show');
+        }
+        function closeModalThird() {
+            modal3.classList.add('hide');
+            modal3.classList.remove('show');
+        }
+
+        modalCloseBtn1.addEventListener('click', closeModalFirst);
+        modalCloseBtn2.addEventListener('click', closeModalSecond);
+        modalCloseBtn3.addEventListener('click', closeModalThird);
+
+
+    //
+
+    function allSumm () { 
+        let userSum = investInput.value, 
+            total = 238000, 
+            sum; 
+        if (userSum >= 0) { 
+             sum = total - userSum; 
+        } 
+        totalSum.textContent = `$${sum}`; 
+    }
+
+    //closing modal after investment
+
+    investedTrigger1.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = event.target;
+            closeModalFirst(); 
+            allSumm(); 
+            investedTextFirst.classList.remove('hide'); 
+            investedTextFirst.classList.add('show'); 
         
+    });
+    investedTrigger2.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = event.target;
+            closeModalSecond(); 
+            allSumm(); 
+            investedTextSecond.classList.remove('hide'); 
+            investedTextSecond.classList.add('show'); 
+        
+    });
+    investedTrigger3.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = event.target;
+            closeModalThird(); 
+            allSumm(); 
+            investedTextThird.classList.remove('hide'); 
+            investedTextThird.classList.add('show'); 
+        
+    });
 
-
-investBtn.classList.remove('first', 'second', 'third');
-
-
-investedTextFirst = document.querySelector('[data-invested1]'), 
-          investedTextSecond = document.querySelector('[data-invested2]'), 
-          investedTextThird = document.querySelector('[data-invested3]'),
-
-
-          investBtn.addEventListener('click', (event) => { 
-            event.preventDefault(); 
-            const target = event.target; 
-            if (target && target.classList.contains('first')) { 
-                closeModal(); 
-                allSumm(); 
-                investedTextFirst.classList.remove('hide'); 
-                investedTextFirst.classList.add('show'); 
-            } else if (target && target.classList.contains('second')) { 
-                closeModal(); 
-                allSumm(); 
-                investedTextSecond.classList.add('show'); 
-                investedTextSecond.classList.remove('hide'); 
-            } else if (target && target.classList.contains('third')) { 
-                closeModal(); 
-                allSumm(); 
-                investedTextThird.classList.add('show'); 
-                investedTextThird.classList.remove('hide'); 
-            } 
-        });
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,121 +1,160 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    //modal
+    //parse JSON into JS
 
-    const modalTriggerFirst = document.querySelector('[data-modal-1]'), 
-          modalTriggerSecond = document.querySelector('[data-modal-2]'), 
-          modalTriggerThird = document.querySelector('[data-modal-3]'),
-          modal = document.querySelector('.modal'),
-          modalCloseBtn = document.querySelector('[data-close]'),
-          investedTrigger = document.querySelector('[data-invested]'),
-          investInput = document.querySelector('.modal__input'),
-          totalSum = document.querySelector('.investment-amount'),
-          investedTextFirst = document.querySelector('[data-invested-1]'), 
-          investedTextSecond = document.querySelector('[data-invested-2]'), 
-          investedTextThird = document.querySelector('[data-invested-3]');
-
-        modalTriggerFirst.addEventListener('click', () => { 
-            modal.classList.add('show'); 
-            modal.classList.remove('hide'); 
-            investedTrigger.classList.add('first'); 
-        }); 
-     
-        modalTriggerSecond.addEventListener('click', () => { 
-            modal.classList.add('show'); 
-            modal.classList.remove('hide'); 
-            investedTrigger.classList.add('second'); 
-        }); 
-        modalTriggerThird.addEventListener('click', () => { 
-            modal.classList.add('show'); 
-            modal.classList.remove('hide'); 
-            investedTrigger.classList.add('third'); 
-        });
-
-    function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        document.body.style.overflow = '';
-        investedTrigger.classList.remove('first', 'second', 'third');
-    }
-    modalCloseBtn.addEventListener('click', closeModal);
-
-
-    function allSumm () { 
-        let userSum = investInput.value, 
-            total = 238000, 
-            sum; 
-        if (userSum >= 0) { 
-             sum = total - userSum; 
-        } 
-        totalSum.textContent = `$${sum}`; 
-    }
-    investedTrigger.addEventListener('click', (event) => { 
-        event.preventDefault(); 
-        const target = event.target; 
-        if (target && target.classList.contains('first')) { 
-            closeModal(); 
-            allSumm(); 
-            investedTextFirst.classList.remove('hide'); 
-            investedTextFirst.classList.add('show'); 
-        } else if (target && target.classList.contains('second')) { 
-            closeModal(); 
-            allSumm(); 
-            investedTextSecond.classList.add('show'); 
-            investedTextSecond.classList.remove('hide'); 
-        } else if (target && target.classList.contains('third')) { 
-            closeModal(); 
-            allSumm(); 
-            investedTextThird.classList.add('show'); 
-            investedTextThird.classList.remove('hide'); 
-        } 
-    });
-
-//seconds to days converter
-
-    function secondsToDhms(seconds) {
-        seconds = Number(seconds);
-        var d = Math.floor(seconds / (3600*24));
-        var h = Math.floor(seconds % (3600*24) / 3600);
-        var m = Math.floor(seconds % 3600 / 60);
-        var s = Math.floor(seconds % 60);
-        
-        var dDisplay = d > 0 ? d + (d == 1 ? " day " : " days ") : "";
-        var hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
-        var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
-        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-        return dDisplay + hDisplay + mDisplay + sDisplay;
-        }
-
-//parse JSON into JS
-
-    $.getJSON("js/current-loans.json", function(data) { 
+      $.getJSON("js/current-loans.json", function(data) { 
         console.log(data); 
- 
+
         $('.item-name.first').html(data.loans[0].title); 
-        $('.modal__loan-title').html(data.loans[0].title); 
+        $('.modal__loan-title-first').html(data.loans[0].title); 
         $('.item-name.second').html(data.loans[1].title); 
+        $('.modal__loan-title-second').html(data.loans[1].title);
         $('.item-name.third').html(data.loans[2].title); 
-        $('.item-descr.first span').html(data.loans[0].tranche); 
-        $('.item-descr.second span').html(data.loans[1].tranche);
-        $('.item-descr.third span').html(data.loans[2].tranche);
+        $('.modal__loan-title-third').html(data.loans[2].title);
+        $('.item-descr.first span').html(data.loans[0].available); 
+        $('.item-descr.second span').html(data.loans[1].available);
+        $('.item-descr.third span').html(data.loans[2].available);
         $('.item-descr.first span1').html(data.loans[0].ltv); 
         $('.item-descr.second span1').html(data.loans[1].ltv); 
         $('.item-descr.third span1').html(data.loans[2].ltv); 
-
-        $('.modal__available span').html(data.loans[0].available); 
-        $('.modal__available span').html(data.loans[0].available); 
+        $('.modal__available-1 span').html(data.loans[0].available); 
+        $('.modal__available-2 span').html(data.loans[1].available);
+        $('.modal__available-3 span').html(data.loans[2].available);
         
-        const modalTimer = document.querySelector('.modal__timer span');
-        let newData = data.loans[0].term_remaining;
-        secondsToDhms(newData);
+    //seconds to days converter
 
-        modalTimer.innerHTML = secondsToDhms(newData);
+        function secondsToDhms(seconds) {
+            seconds = Number(seconds);
+            var d = Math.floor(seconds / (3600*24));
+            var h = Math.floor(seconds % (3600*24) / 3600);
+            var m = Math.floor(seconds % 3600 / 60);
+            var s = Math.floor(seconds % 60);
+            
+            var dDisplay = d > 0 ? d + (d == 1 ? " day " : " days ") : "";
+            var hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
+            var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+            var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+            return dDisplay + hDisplay + mDisplay + sDisplay;
+            }
+    
+    // convert json term remaining data into modal
 
-        function toggleModalContent() {
-
-        }
-
+        const modalTimer1 = document.querySelector('.modal__timer-1 span'),
+              modalTimer2 = document.querySelector('.modal__timer-2 span'),
+              modalTimer3 = document.querySelector('.modal__timer-3 span');
+        
+        modalTimer1.innerHTML = `${secondsToDhms(data.loans[0].term_remaining)}`;
+        modalTimer2.innerHTML = `${secondsToDhms(data.loans[1].term_remaining)}`;
+        modalTimer3.innerHTML = `${secondsToDhms(data.loans[2].term_remaining)}`;
     });
 
+//modal
+
+    const   modalTriggerFirst = document.querySelector('[data-btn-1]'), 
+            modalTriggerSecond = document.querySelector('[data-btn-2]'), 
+            modalTriggerThird = document.querySelector('[data-btn-3]'),
+            modal1 = document.querySelector('[data-modal-1]'),
+            modal2 = document.querySelector('[data-modal-2]'),
+            modal3 = document.querySelector('[data-modal-3]'),
+            modalCloseBtn1 = document.querySelector('[data-close-1]'),
+            modalCloseBtn2 = document.querySelector('[data-close-2]'),
+            modalCloseBtn3 = document.querySelector('[data-close-3]'),
+            investedTrigger1 = document.querySelector('[data-investbtn-1]'),
+            investedTrigger2 = document.querySelector('[data-investbtn-2]'),
+            investedTrigger3 = document.querySelector('[data-investbtn-3]'),
+            investInput = document.querySelector('.modal__input'),
+            totalSum = document.querySelector('.investment-amount'),
+            investedTextFirst = document.querySelector('[data-invested-1]'), 
+            investedTextSecond = document.querySelector('[data-invested-2]'), 
+            investedTextThird = document.querySelector('[data-invested-3]');
+
+
+    //show modal
+
+    modalTriggerFirst.addEventListener('click', () => { 
+        modal1.classList.add('show'); 
+        modal1.classList.remove('hide'); 
+    }); 
+ 
+    modalTriggerSecond.addEventListener('click', () => { 
+        modal2.classList.add('show'); 
+        modal2.classList.remove('hide'); 
+    }); 
+    modalTriggerThird.addEventListener('click', () => { 
+        modal3.classList.add('show'); 
+        modal3.classList.remove('hide'); 
+    });
+
+    //close modal
+
+    function closeModalFirst() {
+        modal1.classList.add('hide');
+        modal1.classList.remove('show');
+    }
+    function closeModalSecond() {
+        modal2.classList.add('hide');
+        modal2.classList.remove('show');
+    }
+    function closeModalThird() {
+        modal3.classList.add('hide');
+        modal3.classList.remove('show');
+    }
+
+    modalCloseBtn1.addEventListener('click', closeModalFirst);
+    modalCloseBtn2.addEventListener('click', closeModalSecond);
+    modalCloseBtn3.addEventListener('click', closeModalThird);
+
+
+//calculating amount of investment that user put out of total amount
+
+function allSumm () { 
+    let userSum = investInput.value, 
+        total = 238000, 
+        sum; 
+    if (userSum >= 0) { 
+         sum = total - userSum; 
+    } 
+    totalSum.textContent = `$${sum}`; 
+}
+
+//closing modal after investment
+
+investedTrigger1.addEventListener('click', (event) => {
+    event.preventDefault();
+    const target = event.target;
+    if (investInput.value > 0) {
+        closeModalFirst(); 
+        allSumm(); 
+        investedTextFirst.classList.remove('hide'); 
+        investedTextFirst.classList.add('show'); 
+    } else {
+        alert('Put any amount to invest');
+    }
+});
+
+investedTrigger2.addEventListener('click', (event) => {
+    event.preventDefault();
+    const target = event.target;
+    if (investInput.value > 0) {
+        closeModalSecond(); 
+        allSumm(); 
+        investedTextSecond.classList.remove('hide'); 
+        investedTextSecond.classList.add('show'); 
+    } else {
+        alert('Put any amount to invest');
+    }
+});
+investedTrigger3.addEventListener('click', (event) => {
+    event.preventDefault();
+    const target = event.target;
+    if (investInput.value > 0) {
+        closeModalThird(); 
+        allSumm(); 
+        investedTextThird.classList.remove('hide'); 
+        investedTextThird.classList.add('show'); 
+    } else {
+        alert('Put any amount to invest');
+    }
+});
 
 });
